1874780e29a187426075675718a20f6a549d8caf
NGOC HOA BUI <buingochoa1987@gmail.com>
2019-01-06 13:19:41 +1100

    add domian entities

---
+++ WebShop.Model/Abstract/Auditable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebShop.Model.Abstract
+{
+    public abstract class Auditable : IAuditable
+    {
+        public DateTime? CreateDate { set; get; }
+
+        [MaxLength(256)]
+        public string CreatedBy { set; get; }
+
+        [MaxLength(256)]
+        public DateTime? UpdateDate { set; get; }
+
+        public string UpdatedBy { set; get; }
+
+        [MaxLength(256)]
+        public string MetaKeyword { set; get; }
+
+        [MaxLength(256)]
+        public string MetaDescription { set; get; }
+
+        public bool Status { set; get; }
+    }
+}
