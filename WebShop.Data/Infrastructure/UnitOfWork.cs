100693bb3ba66dc1ea69d4f9a0e367229781147f
NGOC HOA BUI <buingochoa1987@gmail.com>
2019-01-07 22:32:09 +1100

    Finish

---
+++ WebShop.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop.Data.Infrastructure
+{
+    class UnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private WebShopDbContext dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public WebShopDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
